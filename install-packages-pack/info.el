7dc3474de45110574cd2ed143b9801bdfc190e93
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-15 20:31:14 +0200

    Add install-packages-pack as a git submodule.

--- install-packages-pack/info.el
+++
@@ -1,3 +0,0 @@
-(live-pack-name "install-packs-pack")
-(live-pack-version "0.0.1")
-(live-pack-description "install-packs-pack - a specific pack to help in installing other elpa/melpa/marmalade packs")
