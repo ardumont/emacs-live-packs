2fdb81f4c6ba6fba64d17ee795b9bc45aca01a08
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-15 20:32:37 +0200

    Add browser-pack as a git submodule.

--- browser-pack/info.el
+++
@@ -1,3 +0,0 @@
-(live-pack-name "browser-pack")
-(live-pack-version "0.0.1")
-(live-pack-description "browser-pack - setup around browser")
--- browser-pack/init.el
+++
@@ -1,8 +0,0 @@
-(require 'browse-url)
-
-(setq browse-url-browser-function 'browse-url-firefox
-      browse-url-generic-program "/usr/bin/firefox")
-
-;; (setq browse-url-browser-function 'browse-url-generic
-;;       browse-url-generic-program "/usr/bin/google-chrome"
-;;       browse-url-generic-args '("--no-proxy-server"))
