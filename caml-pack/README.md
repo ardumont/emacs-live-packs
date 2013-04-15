047980a63333379259327d48f602ea0fbc19fe09
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-15 20:33:33 +0200

    Add caml-pack as a git submodule.

--- caml-pack/README.md
+++
@@ -1,3 +0,0 @@
-## User Caml Pack
-
-A personal caml pack to ease the coding in caml environment
--- caml-pack/info.el
+++
@@ -1,3 +0,0 @@
-(live-pack-name "caml-pack")
-(live-pack-version "0.0.1alpha")
-(live-pack-description "caml-pack")
