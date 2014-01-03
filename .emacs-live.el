cd0be5c6b7e535cb3dd1e7856f5d07a5b68075e0
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-04 00:35:59 +0100

    Refactor - Improve the .emacs-live.el file

--- .emacs-live.el
+++ .emacs-live.el
@@ -1,47 +1,33 @@
-;; a utility function to help in installing emacs-live packs
-(defun add-live-packs (path packs)
-  "Adding live packs at startup"
-  (live-add-packs
-   (mapcar
-    (lambda (pack)
-      (concat path pack))
-    packs)))
+;; File loaded automatically the default emacs-live packs
 
-;; all the other packs needed
-(add-live-packs "~/.emacs-live-packs/"
-                '(;; installing emacs repositories (melpa, milkbox, marmalade) and the install-packs utility function
-                  "install-packages-pack"
-                  ;; "el-get-pack"
-                  "buffer-pack"
-                  ;; a pack to mess around until drying stuff
-                  "scratch-pack"
-                  ;; a setup blog pack for wordpress
-                  "blog-pack"
-                  "haskell-pack"
-                  ;; setup org-mode
-                  "orgmode-pack"
-                  "lisp-pack"
-                  "git-pack"
-                  ;; setup mail credentials
-                  "mail-pack"
-                  "shell-pack"
-                  "browser-pack"
-                  ;; irc credential setup
-                  "chat-pack"
-                  "clojure-pack"
-                  ;; nrepl override bindings from nrepl
-                  "nrepl-pack"
-                  "clojurescript-pack"
-                  "caml-pack"
-                  ;; adding stuff regarding emacs modeline
-                  "modeline-pack"
-                  "twitter-pack"
-                  "puppet-pack"
-                  ;; chrome editing textarea using emacs
-                  "chrome-pack"
-                  "macro-pack"
-                  "scala-pack"
-                  "elisp-pack"
-;;                  "groovy-pack"
-                  "php-pack"
-                  "ctags-pack"))
+(defun emacs-live-packs/add-live-packs (path packs) "Utility function to help in installing emacs-live-packs (bunch of user packs)"
+  (live-add-packs (mapcar (lambda (pack) (concat path pack)) packs)))
+
+(emacs-live-packs/add-live-packs "~/.emacs-live-packs/"
+                                 '("install-packages-pack";; installing emacs repositories (melpa, milkbox, marmalade) and the install-packs utility function
+                                   ;; "el-get-pack"
+                                   "buffer-pack"          ;; a pack to mess around until drying stuff
+                                   "scratch-pack"
+                                   "blog-pack"            ;; a setup blog pack for wordpress
+                                   "haskell-pack"
+                                   "orgmode-pack"         ;; setup org-mode
+                                   "lisp-pack"
+                                   "git-pack"
+                                   "mail-pack"            ;; setup mail credentials
+                                   "shell-pack"
+                                   "browser-pack"
+                                   "chat-pack"            ;; irc credential setup
+                                   "clojure-pack"
+                                   "nrepl-pack"           ;; nrepl override bindings from nrepl
+                                   "clojurescript-pack"
+                                   "caml-pack"
+                                   "modeline-pack"        ;; adding stuff regarding emacs modeline
+                                   "twitter-pack"
+                                   "puppet-pack"
+                                   "chrome-pack"          ;; chrome editing textarea using emacs
+                                   "macro-pack"
+                                   "scala-pack"
+                                   "elisp-pack"
+                                   ;; "groovy-pack"
+                                   "php-pack"
+                                   "ctags-pack"))
