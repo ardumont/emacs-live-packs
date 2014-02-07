ac3c621a2a26d267f581e33d66de4ddea0a4e0cd
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-07 09:55:14 +0100

    Use the new beta-24

--- .emacs-live.el
+++ .emacs-live.el
@@ -1,7 +1,5 @@
 ;; File loaded automatically the default emacs-live packs
 
-(live-use-dev-packs)
-
 (defun emacs-live-packs/add-live-packs (path packs) "Utility function to help in installing emacs-live-packs (bunch of user packs)"
   (live-add-packs (mapcar (lambda (pack) (concat path pack)) packs)))
 
@@ -34,4 +32,3 @@
                                    "php-pack"
                                    "ctags-pack"
                                    "prelude-pack"))
-
