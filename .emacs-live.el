7213b3a15714d4117bcadf67f105ee081a4675e5
Antoine R. Dumont <eniotna.t@gmail.com>
2015-05-26 23:02:55 +0200

    Install pack's code inside the dedicated pack

--- .emacs-live.el
+++ .emacs-live.el
@@ -38,93 +38,60 @@
 ;;
 
 (defun emacs-live-packs/add-live-packs (parent-dir)
- "Add all level PARENT-DIR subdirs to the `load-path'."
- (dolist (f (directory-files parent-dir))
-   (let ((name (expand-file-name f parent-dir)))
-     (when (and (file-directory-p name)
-                (not (string-prefix-p "." f)))
-       (add-to-list 'load-path name)
-       (emacs-live-packs/add-live-packs name)))))
+  "Add all level PARENT-DIR subdirs to the `load-path'."
+  (dolist (f (directory-files parent-dir))
+    (let ((name (expand-file-name f parent-dir)))
+      (when (and (file-directory-p name)
+                 (not (string-prefix-p "." f)))
+        (add-to-list 'load-path name)
+        (emacs-live-packs/add-live-packs name)))))
+
+(defvar emacs-live-packs/home "~/.emacs-live-packs"
+  "Home of the emacs-live-packs.")
 
 (emacs-live-packs/add-live-packs emacs-live-packs/home)
-
-(defun live-load-pack (pack-dir)
-  "Load a live pack. This is a dir that could contain either the
-  files info.el and init.el or `pack-name`.el. Adds the packs's
-  dir and lib dir to the load-path"
-  (message (concat "\n\n==> Loading Emacs Live Pack: " pack-dir))
-  (let* ((pack-name        (file-name-base (substring pack-dir 0 (1- (length pack-dir))))) ;; hurk
-         (pack-dir         (file-name-as-directory pack-dir))
-         (pack-info        (concat pack-dir "info.el"))
-         (maybe-pack-init  (concat pack-dir "init.el")) ;; default init in emacs-live
-         (maybe-pack-init2 (concat pack-dir pack-name ".el")) ;; I no longer use emacs-live's convention but emacs'
-         (pack-init        (if (file-exists-p maybe-pack-init) maybe-pack-init maybe-pack-init2)))
-    (setq live-current-pack-dir pack-dir)
-    (live-clear-pack-info)
-    (when (file-exists-p pack-info)
-      (progn
-        (load-file pack-info)
-        (live-print-pack-info)))
-    (add-to-list 'load-path pack-dir)
-    (add-to-list 'load-path (live-pack-lib-dir))
-    (when (file-exists-p pack-init)
-      (load-file pack-init))
-    (setq live-current-pack-dir nil)))
-
-(defun emacs-live-packs/add-live-packs (path packs)
-  "Utility function to help in installing emacs-live-packs (bunch of user packs)"
-  (live-add-packs (mapcar (lambda (pack)
-                            (expand-file-name (format "%s/%s" path pack)))
-                          packs)));; let emacs-live load the pack
-
-;; will need this for forcing the loading of the packs.
-(setq custom-file "~/.emacs-live-packs/load-packs.el")
 
 ;;
 ;; Edit this part as you please (just keep the install-packages-pack)
 ;;
-(defvar emacs-live-packs/home "~/.emacs-live-packs"
-  "Home of the emacs-live-packs.")
 
 (defvar emacs-live-packs/user-packs '("install-packages-pack"
                                       "theme-pack"
-                                      ;;                                   "el-get-pack"
+                                      ;; "el-get-pack"
                                       "buffer-pack"
                                       "scratch-pack"
-                                      "blog-pack"
-                                      "haskell-pack"
+                                      ;; "blog-pack"
+                                      ;; "haskell-pack"
                                       "orgmode-pack"
                                       "lisp-pack"
                                       "git-pack"
                                       "mail-pack"
                                       "shell-pack"
                                       "browser-pack"
-                                      "chat-pack"
-                                      "clojure-pack"
-                                      ;;                                   "nrepl-pack"
-                                      "clojurescript-pack"
-                                      "caml-pack"
+                                      ;; "chat-pack"
+                                      ;; "clojure-pack"
+                                      ;; "nrepl-pack"
+                                      ;; "clojurescript-pack"
+                                      ;; "caml-pack"
                                       "modeline-pack"
                                       "twitter-pack"
                                       "puppet-pack"
-                                      ;;                                   "chrome-pack"
+                                      ;; "chrome-pack"
                                       "macro-pack"
-                                      "scala-pack"
+                                      ;; "scala-pack"
                                       "elisp-pack"
-                                      "groovy-pack"
-                                      "php-pack"
-                                      ;;                                   "ctags-pack"
+                                      ;; "groovy-pack"
+                                      ;; "php-pack"
+                                      ;; "ctags-pack"
                                       "prelude-live-pack"
-                                      "stumpwm-pack"
-                                      "pres-pack")
+                                      ;; "stumpwm-pack"
+                                      ;; "pres-pack"
+                                      )
   "User's packs. Feel free to comment out the ones you do not want.")
 
 ;;
 ;; Read only
 ;;
-
-;; (emacs-live-packs/add-live-packs emacs-live-packs/home
-;;                                  emacs-live-packs/user-packs)
 
 (mapc (lambda (pack-name-str)
         (require (intern pack-name-str) nil 'no-error)) emacs-live-packs/user-packs)
